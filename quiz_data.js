e6df4616fc6a262aacc3e6746a79a3792f7bf6d3
sangeetapatidar <sppatidar55y@gmail.com>
2025-07-08 19:37:56 +0530

    Add quiz_data.js

---
+++ quiz_data.js
@@ -0,0 +1,7 @@
+const quizQuestions = {
+  "A": [
+    { "question": "Affidavit का सही हिंदी अर्थ क्या है?", "options":["घोषणापत्र","अनुबंध","वसीयतनामा","रिपोर्ट"], "answer":"घोषणापत्र" },
+    // total 10 questions per letter
+  ],
+  // "B": [...], … तिथि तक "Z"
+};
